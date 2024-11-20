f94f9fcbc56ae9c74d2b35d9214630d744525704
abhi114 <nabhi531@gmail.com>
2024-11-20 22:33:21 +0530

    screen ui fixed

--- src/app/components/Messages.tsx
+++ src/app/components/Messages.tsx
@@ -1,5 +1,6 @@
 import {type Message as TMessage} from "ai/react" //you can use type to import only the type definition to reduce bundle size
 import { Message } from "./Message"
+import { MessageSquare } from "lucide-react"
 interface MessagesProps {
     messages : TMessage[],
 }
@@ -7,11 +8,13 @@ interface MessagesProps {
 export const Messages = ({messages}:MessagesProps)=>{
         return (
             <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
-                {messages ? messages.map((message,i)=>(
+                {messages.length ? messages.map((message,i)=>(
                     <Message key={i} content={message.content} isUserMessage={message.role === 'user'}/>
                 )):
-                (<div>
-
+                (<div className="flex-1 flex flex-col items-center justify-center gap-2">
+                    <MessageSquare className="size-8 text-blue-500"/>
+                    <h3 className="font-semibold text-xl text-white">You're all set!</h3>
+                    <p className="text-zinc-500 text-sm">ask your first question to get started...</p>
                 </div>)} 
             </div>
         )
